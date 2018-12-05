41d023b4c005f7969e1f54904035c77417ade2c6
Ladoo <souravkumar54325@gmail.com>
2018-12-05 15:19:13 +0530

    Initial commit

---
+++ app/src/main/java/com/errorfoundtechnologies/uiexample/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.errorfoundtechnologies.uiexample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        b1.setOnClickListener {
+            tv1.text=et1.text
+        }
+    }
+}
